20aff9cd89ccbdb3631b83a93484916eeab50027
Mike <mikhail-gankin@yandex.ru>
2026-07-30 13:34:28 +0300

    refactor sunmar image

--- src/components/sunmar-image/sunmar-image.ts
+++ src/components/sunmar-image/sunmar-image.ts
@@ -5,12 +5,18 @@ import styles from './sunmar-image.scss?inline';
 
 export const SUNMAR_IMAGE_TAG_NAME = 'sunmar-image';
 
+export type SunmarImageLoading = 'eager' | 'lazy';
+
 export class SunmarImage extends LitElement {
   static properties = {
     src: { type: String },
     srcset: { type: String },
+    sizes: { type: String },
     media: { type: String },
-    alt: { type: String }
+    alt: { type: String },
+    width: { type: Number },
+    height: { type: Number },
+    loading: { type: String }
   };
 
   static styles = [componentBaseStyles, css`
@@ -19,12 +25,18 @@ export class SunmarImage extends LitElement {
 
   src = '';
   srcset = '';
+  sizes = '';
   media = '(min-width: 768px)';
   alt = '';
+  width?: number;
+  height?: number;
+  loading?: SunmarImageLoading;
 
   protected render() {
     const srcset = this.srcset.trim();
+    const sizes = this.sizes.trim();
     const media = this.media.trim();
+    const src = this.src.trim();
 
     return html`
       <picture class="picture" part="picture">
@@ -32,11 +44,20 @@ export class SunmarImage extends LitElement {
           ? html`
               <source
                 srcset=${srcset}
+                sizes=${ifDefined(sizes || undefined)}
                 media=${ifDefined(media || undefined)}
               />
             `
           : nothing}
-        <img class="img" part="img" src=${this.src} alt=${this.alt} />
+        <img
+          class="img"
+          part="img"
+          src=${ifDefined(src || undefined)}
+          alt=${this.alt}
+          width=${ifDefined(this.width)}
+          height=${ifDefined(this.height)}
+          loading=${ifDefined(this.loading)}
+        />
       </picture>
     `;
   }
